8baba15e7d1fd5ba54585b2db7e1c99f84b7f507
AndreyErin <eaa.forwork@gmail.com>
2025-05-13 17:22:45 +0700

    В методе расширения TryReadData() для FileRepository имя параметра отвечающее за класс самого репозитория было отброшено(заменено на _), т.к. не использовалось в самомо методе. Try-catch теперь перехватывает ошибки связанные только с десериализацией.

--- AdvertisingPlatforms.DAL/FileAccess/Extensions/RepositoryReaderExtensions.cs
+++ AdvertisingPlatforms.DAL/FileAccess/Extensions/RepositoryReaderExtensions.cs
@@ -10,27 +10,34 @@ namespace AdvertisingPlatforms.DAL.FileAccess.Extensions
         /// Checking the possibility to get the data.
         /// </summary>
         /// <typeparam name="TResource">Resource</typeparam>
-        /// <param name="repositoryReader"></param>
+        /// <param name="_"></param>
         /// <param name="filePath">Path for file.</param>
         /// <param name="data">Data from repository.</param>
         /// <returns></returns>
-        public static bool TryReadData<TResource>(this RepositoryReader repositoryReader, string filePath,[NotNullWhen(true)] out List<TResource>? data) where TResource : Resource
+        public static bool TryReadData<TResource>(this RepositoryReader _, string filePath, [NotNullWhen(true)] out List<TResource>? data) where TResource : Resource
         {
             data = null;
 
+            using StreamReader sr = new StreamReader(Path.Combine(AppContext.BaseDirectory, filePath));
+            var jsonDb = sr.ReadToEnd();
+
+            if (string.IsNullOrEmpty(jsonDb))
+            {
+                
+                return false;
+            }
+
             try
             {
-                using StreamReader sr = new StreamReader(Path.Combine(AppContext.BaseDirectory, filePath));
-                var jsonDb = sr.ReadToEnd();
-
-                if (string.IsNullOrEmpty(jsonDb))
-                    return false;
-
                 data = JsonSerializer.Deserialize<List<TResource>>(jsonDb);
 
                 return data != null;
             }
-            catch (Exception)
+            catch (JsonException)
+            {
+                return false;
+            }
+            catch (NotSupportedException)
             {
                 return false;
             }
